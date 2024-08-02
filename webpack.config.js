74035c9cc731cd7a6e0274b78dd264bdca632c94
HP <vai.katia03@mail.ru>
2024-08-02 13:53:36 +0300

    fix: configuration publicPath in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,71 +2,77 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/art-museum-react/',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    alias: {
-      process: 'process/browser',
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@utils': path.resolve(__dirname, 'src/utils/'),
-      '@styles': path.resolve(__dirname, 'src/styles/'),
-      '@assets': path.resolve(__dirname, 'src/assets/'),
-      '@pages': path.resolve(__dirname, 'src/pages/'),
-      '@constants': path.resolve(__dirname, 'src/constants/'),
-      '@store': path.resolve(__dirname, 'src/store/'),
+module.exports = (env) => {
+  const isProd = env.production;
+
+  return {
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+      publicPath: isProd ? '/art-museum-react/' : '/',
     },
-    modules: ['.', 'node_modules'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      alias: {
+        process: 'process/browser',
+        '@components': path.resolve(__dirname, 'src/components/'),
+        '@utils': path.resolve(__dirname, 'src/utils/'),
+        '@styles': path.resolve(__dirname, 'src/styles/'),
+        '@assets': path.resolve(__dirname, 'src/assets/'),
+        '@pages': path.resolve(__dirname, 'src/pages/'),
+        '@constants': path.resolve(__dirname, 'src/constants/'),
+        '@store': path.resolve(__dirname, 'src/store/'),
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+      modules: ['.', 'node_modules'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[path][name].[ext]',
-            },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
           },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'public/index.html',
-      filename: 'index.html',
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[path][name].[ext]',
+              },
+            },
+          ],
+        },
+      ],
     },
-    compress: true,
-    port: 3000,
-    historyApiFallback: true,
-  },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        filename: 'index.html',
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      compress: true,
+      port: 3000,
+      historyApiFallback: true,
+    },
+    devtool: isProd ? 'source-map' : 'inline-source-map',
+    mode: isProd ? 'production' : 'development',
+  };
 };
